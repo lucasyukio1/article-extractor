b6dfa1623fe3b060ec6875c4c1d2871b5bd82311
Lucas Yukio Wahasugui Barros <lucaswhs@gmail.com>
2023-09-05 17:33:59 -0300

    Front-end pré integração com API

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,31 @@
+import { logo } from '../assets';
 import React from 'react'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <header className="w-full flex justify-center items-center flex-col">
+      <nav className='flex justify-between items-center w-full mb-10 pt-3'>
+        <img src={logo} alt='sumz_logo' className='w-28 object-contain' />
+
+        <button
+          type='button'
+          onClick={() =>
+            window.open("https://github.com/yokaiprod/article-extractor", "_blank")
+          }
+          className='black_btn'
+        >
+          GitHub
+        </button>
+      </nav>
+
+      <h1 className="head_text">
+        Resuma artigos utilizando a tecnologia <br className="max-md:hidden" />
+        <span className="orange_gradient">OpenAI GPT-4</span>
+      </h1>
+      <h2 className="desc">
+        Simplifique sua leitura com Sumz, um resumidor de artigos de código aberto que transforma artigos extensos em resumos claros e concisos
+      </h2>
+    </header>
   )
 }
 
