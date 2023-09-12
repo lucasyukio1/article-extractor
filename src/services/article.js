6c26efaa44d3d6001cf9c74b1a9c5530b5389664
Lucas Yukio Wahasugui Barros <lucaswhs@gmail.com>
2023-09-12 10:20:49 -0300

    finalização do front-end

--- src/services/article.js
+++ src/services/article.js
@@ -15,7 +15,7 @@ export const articleApi = createApi({
     }),
     endpoints: (builder) => ({
         getSummary: builder.query({
-            query: (params) => `/summarize?url=${encodeURIComponent(params.articleUrl)}&length=5`
+            query: (params) => `/summarize?url=${encodeURIComponent(params.articleUrl)}&length=5&lang=pt`
         })
     }) 
 });
